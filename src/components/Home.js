1a81b187ce8ce6a5f079871f65463cf5e75b0cf7
Mukesh Kr singh <mukesh.puppy@gmail.com>
2019-01-10 07:53:12 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+/*import React from 'react';
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    My Home page!
+  </div>
+);
+
+export default Home;
+*/
+import React from 'react';
+class Home extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Home...</h1>
+         </div>
+      )
+   }
+}
+
+export default Home;
